1651062cefe85916253b1ce10fedde51f422c375
itcast <test@itcast.cn>
2016-12-12 17:36:13 +0800

    正在热映使用本地数据展示列表功能

--- app/in_theaters/module.js
+++ app/in_theaters/module.js
@@ -10,8 +10,35 @@
 			});
 
 		}])
-		.controller('InTheatersController', ['$scope', function($scope){
+		.controller('InTheatersController', ['$scope', '$http', function($scope, $http){
 			// 功能 先不实现
+			// 数据应该是从 豆瓣电影API 中获取到
+			
+			// Promise ES6 中提到一个解决 回调地狱 的问题
+			
+			// 1 使用本地json文件模拟数据的获取
+			// 		angular中通过服务 $http 来发送ajax请求
+			$http({
+				method: 'GET',
+				url: './in_theaters/data.json'
+			}).then(function(response) {
+				// ajax请求发送成功的回调函数
+				// console.log('成功！')
+				console.log(response)
+				$scope.movie = response.data;
+			}, function(response) {
+				// 失败的函数
+				console.log('失败！')
+				console.log(response)
+			});
+
+			/*$http.get('./in_theaters/data.json')
+					 .success(function(response) {
+					 	console.log(response)
+						$scope.movie = response;
+					 })
+					 .error(function(res){})*/
+
 		}]);
 
 })(angular); 
